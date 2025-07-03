f12c4591a86ff41fc4c77e80704cd73bdc4cf79d
Haim Kastner <148038767+chkp-haimk@users.noreply.github.com>
2025-07-03 19:59:27 +0300

    Dependency Alignment (requirements.txt/setup.py) (#23)
    Continuation of [Issue #19](https://github.com/CheckPointSW/harmony-endpoint-management-py-sdk/issues/19)
    
    * Changed `certifi` requirement to `>=2024.0,<2026.0` to allow for better compatibility
    
    * Updated `typing-extensions` to `~4.9.0`
    
    * Synced `requirements.txt` to `setup.py` (previously mismatched)
    
    Cleaned up some code to conform with `PEP8`

--- chkp_harmony_endpoint_management_sdk/core/harmony_endpoint.py
+++ chkp_harmony_endpoint_management_sdk/core/harmony_endpoint.py
@@ -3,13 +3,16 @@
 from chkp_harmony_endpoint_management_sdk.classes.harmony_endpoint_saas_options import HarmonyEndpointSaaSOptions
 from chkp_harmony_endpoint_management_sdk.classes.infinity_portal_auth import InfinityPortalAuth
 from chkp_harmony_endpoint_management_sdk.classes.internal import SessionOperations
-from chkp_harmony_endpoint_management_sdk.classes.on_premise_portal_auth import OnPremisePortalAuth
 from chkp_harmony_endpoint_management_sdk.core.session_manager import SessionManager
 from chkp_harmony_endpoint_management_sdk.generated.cloud import HarmonyEndpointBase as HarmonyEndpointCloudBase
-# from chkp_harmony_endpoint_management_sdk.generated.premise import HarmonyEndpointBase as HarmonyEndpointPremiseBase
 from chkp_harmony_endpoint_management_sdk.generated.saas import HarmonyEndpointBase as HarmonyEndpointSaaSBase
 
+# from chkp_harmony_endpoint_management_sdk.classes.on_premise_portal_auth import OnPremisePortalAuth
+# from chkp_harmony_endpoint_management_sdk.generated.premise import HarmonyEndpointBase as HarmonyEndpointPremiseBase
+
 print_ea_message = True
+
+
 class HarmonyEndpoint(HarmonyEndpointCloudBase):
 
     def __init__(self):
@@ -18,10 +21,13 @@ class HarmonyEndpoint(HarmonyEndpointCloudBase):
     def connect(self, infinity_portal_auth: InfinityPortalAuth):
         def keep_alive():
             return self._session_api.keep_alive()
+
         def login():
             return self._session_api.login_cloud()
+
         def job_status(job_id):
-            return self.jobs_api.get_job_by_id(path_params={ 'jobId': job_id })
+            return self.jobs_api.get_job_by_id(path_params={'jobId': job_id})
+
         session_operations = SessionOperations(keep_alive_operation=keep_alive, login_operation=login, job_status_operation=job_status)
         self._session_manager.connect_cloud(infinity_portal_auth, session_operations)
 
@@ -35,11 +41,12 @@ class HarmonyEndpoint(HarmonyEndpointCloudBase):
 #         def keep_alive():
 #             return self._session_api.keep_alive()
 #         def login():
-#             return self._session_api.login_premise(body={ "username": on_premise_portal_auth.username, "password" : on_premise_portal_auth.password })
+#             return self._session_api.login_premise(body={"username": on_premise_portal_auth.username, "password": on_premise_portal_auth.password})
 #         def job_status(job_id):
 #             return self.jobs_api.get_job_by_id(path_params={ 'jobId': job_id })
 #         session_operations = SessionOperations(keep_alive_operation=keep_alive, login_operation=login, job_status_operation=job_status)
 #         self._session_manager.connect_premise(on_premise_portal_auth, session_operations)
+
 
 class HarmonyEndpointSaaS(HarmonyEndpointSaaSBase):
 
@@ -49,8 +56,8 @@ class HarmonyEndpointSaaS(HarmonyEndpointSaaSBase):
     def connect(self, infinity_portal_auth: InfinityPortalAuth, harmony_endpoint_saas_options: HarmonyEndpointSaaSOptions):
         def keep_alive():
             return self._manage_session_api.public_mssp_keep_alive()
+
         def login():
             return self._manage_session_api.public_mssp_login()
         session_operations = SessionOperations(keep_alive_operation=keep_alive, login_operation=login, job_status_operation=None)
         self._session_manager.connect_saas(infinity_portal_auth, harmony_endpoint_saas_options, session_operations)
-
